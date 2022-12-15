6d4d95039c13310b69a70c2c7cc1d7dfe6df5e1a
Eligio Tapiador Jr <etapiador@gmail.com>
2022-12-16 05:21:49 +0800

    add logo and navigation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import HomeCategories from "./components/home-categories/home-categories.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.components";
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+  return <h1>Iam the shop page</h1>;
+};
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <HomeCategories categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
